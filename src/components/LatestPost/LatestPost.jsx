235a6652dec7080417f66297b120b0688dfd7694
unknown <rakibulhasan.seu.cse@gmail.com>
2023-10-21 23:57:09 +0600

    latest post section added

--- src/components/LatestPost/LatestPost.jsx
+++ src/components/LatestPost/LatestPost.jsx
@@ -2,44 +2,58 @@ import React from "react";
 import latest1 from "../../assets/latest1.png";
 import latest2 from "../../assets/latest2.png";
 import latest3 from "../../assets/latest3.png";
+import latest4 from "../../assets/latest4.png";
 import "./LatestProject.css";
 
 const LatestPost = () => {
   return (
     <div className="container py-5">
-      <div className="text-center">
-        <h3>Latest Post</h3>
+      <div class="bordered-text">
+        <div class="border-l left-border"></div>
+        <div class="text">Latest Post</div>
+        <div class="border-r right-border"></div>
       </div>
-      <div className="row g-4">
-        <div className="col-lg-4 card-top">
-          <div>
+      <div className="row g-4 py-5">
+        <div className="col-lg-4 card-top text-center">
+          <div className="img-container">
             <img src={latest1} className="img-fluid card-img" alt="" />
           </div>
-          <div className="border py-3 px-5 card-bottom bg-white">
-            <p className="text-center">HAIR</p>
-            <h3>Vivamus placerat Luctus Neque nec Faucibus</h3>
-            <p>By Sarfraz Jasim 29 July, 2023</p>
+          <div className="border py-3 px-5 text-container bg-white">
+            <div>
+              <p className="text-center">HAIR</p>
+              <h3>Vivamus placerat Luctus Neque nec Faucibus</h3>
+              <p>By Sarfraz Jasim 29 July, 2023</p>
+            </div>
           </div>
         </div>
-        <div className="col-lg-4 card-top">
-          <div>
+        <div className="col-lg-4 card-top text-center">
+          <div className="img-container">
             <img src={latest2} className="img-fluid card-img" alt="" />
           </div>
-          <div className="border py-3 px-5 card-bottom bg-white">
-            <p className="text-center">HAIR</p>
-            <h3>Vivamus placerat Luctus Neque nec Faucibus</h3>
-            <p>By Sarfraz Jasim 29 July, 2023</p>
+          <div className="border py-3 px-5 text-container bg-white">
+            <div>
+              <p className="text-center">MAKE UP</p>
+              <h3>Vivamus placerat Luctus Neque nec Faucibus</h3>
+              <p>By Sarfraz Jasim 29 July, 2023</p>
+            </div>
           </div>
         </div>
-        <div className="col-lg-4 card-top">
-          <div>
+        <div className="col-lg-4 card-top text-center">
+          <div className="img-container">
             <img src={latest3} className="img-fluid card-img" alt="" />
           </div>
-          <div className="border py-3 px-5 card-bottom bg-white">
-            <p className="text-center">HAIR</p>
-            <h3>Vivamus placerat Luctus Neque nec Faucibus</h3>
-            <p>By Sarfraz Jasim 29 July, 2023</p>
+          <div className="border py-3 px-5 text-container bg-white">
+            <div>
+              <p className="text-center">TIPS</p>
+              <h3>Vivamus placerat Luctus Neque nec Faucibus</h3>
+              <p>By Sarfraz Jasim 29 July, 2023</p>
+            </div>
           </div>
+        </div>
+      </div>
+      <div className="row pt-lg-5 mt-lg-5">
+        <div>
+          <img src={latest4} className="img-fluid" alt="" />
         </div>
       </div>
     </div>
